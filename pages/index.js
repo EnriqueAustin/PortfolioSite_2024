a6ecfa660ecefaf355e301de3da87099e91e09c0
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2024-08-04 18:19:47 +0200

    done with home base

--- pages/index.js
+++ pages/index.js
@@ -61,9 +61,16 @@ const Home = () => {
           Particles
         </div>
         {/* avatar img */}
-        <div className='w-full h-full max-w-[737px] max-h-[678px] absolute bottom-32'>
+        <motion.div
+          className='w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate='show'
+          exit='hidden'
+          transition={{ duration:1, ease: 'easeInOut' }}
+        >
           <Avatar />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
