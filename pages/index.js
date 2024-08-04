344c6c34f30a7fc69881343f6988bfe017c38f46
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2024-08-04 16:22:55 +0200

    working on home page

--- pages/index.js
+++ pages/index.js
@@ -15,22 +15,56 @@ const Home = () => {
       <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
         <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
           {/* title */}
-          <h1 className='h1'>
+          <motion.h1
+            className='h1'
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate='show'
+            exit='hidden'
+          >
             Unleashing Potential <br /> Into{' '}
             <span className='text-accent'>Digital Innovation</span>
-          </h1>
+          </motion.h1>
           {/* subtitle */}
-          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
-          Discover a partnership that extends beyond ordinary – where creativity meets technology. At Maxuim Media, we blend artistic vision with technical expertise to deliver exceptional digital solutions.
-          </p>
-          {/* spinny button */}
-          <div className='flex'>
+          <motion.p 
+            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate='show'
+            exit='hidden'
+          >
+            Discover a partnership that extends beyond ordinary – where creativity meets technology. At Maxuim Media, we blend artistic vision with technical expertise to deliver exceptional digital solutions.
+          </motion.p>
+          {/* spinny btn */}
+          <div className='flex justify-center xl:hidden relative'>
             <ProjectsBtn />
           </div>
+          <motion.div 
+            className='hidden xl:flex'
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate='show'
+            exit='hidden'
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}
-      <div>image</div>
+      <div className='w-[1200px] h-full absolute right-0 bottom-0'>
+        {/* bg image */}
+        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
+        
+        </div>
+        {/* part5icles */}
+        <div>
+          Particles
+        </div>
+        {/* avatar img */}
+        <div className='w-full h-full max-w-[737px] max-h-[678px] absolute bottom-32'>
+          <Avatar />
+        </div>
+      </div>
     </div>
   );
 };
