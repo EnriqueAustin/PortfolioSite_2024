b0146b2a077b7f912ec62ba496103a48a753756c
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2024-08-06 23:20:23 +0200

    working in services

--- pages/services/index.js
+++ pages/services/index.js
@@ -11,7 +11,7 @@ import {
 const serviceData = [
   {
     icon: <RxCrop />,
-    title: 'Branding',
+    title: 'Network Security',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
   {
@@ -34,6 +34,11 @@ const serviceData = [
     title: 'SEO',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
+  {
+    icon: <RxCrop />,
+    title: 'Branding',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
 ];
 
 import ServiceSlider from "../../components/ServiceSlider";
@@ -51,15 +56,35 @@ const Services = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
+            <motion.h2
+              className="h2 xl:mt-8"
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
               My services <span className="text-accent">.</span>
-            </h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            </motion.h2>
+            <motion.p
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
               Lorem ipsum dolor set sdjfof f eospefj fes ew f ef we sfsd hgfg f fg fg  rts er fgs fgcgy h s  rx qwrfwa tsr get gxe grexg 5g exgexgerxgegwe xg rw aezfs f w 
-            </p>
+            </motion.p>
           </div>
-          {/* slider */}
-          <ServiceSlider />
+          <motion.div
+            className="w-full xl:max-w-[65%]"
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+          >
+            {/* slider */}
+            <ServiceSlider />
+          </motion.div>
         </div>
       </div>
       <Bulb />
